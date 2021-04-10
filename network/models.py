52b4b81fbb7ca80123d70e6768783abade5559f4
Diego Giraldo <giraldiego@gmail.com>
2021-04-09 21:42:45 -0500

    Add Profile and Post models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,37 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    """A Profile"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        User, related_name="followed_by", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    avatar_url = models.URLField(blank=True)
+    bio = models.TextField(default="No bio...")
+
+    def __str__(self):
+        return str(self.user.username)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def profile_posts(self):
+        pass
+
+
+class Post(models.Model):
+    """A Post"""
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(
+        auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.content)[:20]
+
+    class Meta:
+        ordering = ("-timestamp",)
