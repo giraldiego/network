4322a0dd7236ebd569d6f818639d42ec8e9d2cc3
Diego Giraldo <giraldiego@gmail.com>
2021-04-13 18:07:13 -0500

    Delete compose, posts_view, profiles views

--- network/urls.py
+++ network/urls.py
@@ -6,10 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
 
-    path("compose", views.compose, name="compose"),
-    path("posts", views.posts_view, name="posts_view"),
-    path("profiles/", views.profiles_view, name="profiles_view"),
-
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
